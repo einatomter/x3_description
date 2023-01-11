96ed009c62ead1dde6f2e519a751ee30da7f7552
einatomter <einatomter@gmail.com>
2023-01-11 15:32:41 +0100

    ROV test connecting

---
+++ scripts/unused/test_connect2.py
@@ -0,0 +1,34 @@
+from time import sleep
+
+from asciimatics.effects import Print
+from asciimatics.renderers import Box
+from asciimatics.scene import Scene
+from asciimatics.screen import ManagedScreen
+
+from blueye.sdk import Drone
+
+
+def print_state(screen: ManagedScreen, drone: Drone):
+    """Updates and prints some of the information from the drone"""
+    screen.print_at(f"Lights: {drone.lights * 100 // 255:3d} %", 2, 1)
+
+    screen.print_at(f"Auto-depth: {'On' if drone.motion.auto_depth_active else 'Off':>5}", 2, 3)
+    screen.print_at(f"Auto-heading: {'On' if drone.motion.auto_heading_active else 'Off':>3}", 2, 4)
+
+    screen.print_at(f"Depth: {drone.depth} mm", 2, 6)
+
+    screen.print_at(f"Roll: {drone.pose['roll']:7.2f}°", 2, 8)
+    screen.print_at(f"Pitch: {drone.pose['pitch']:6.2f}°", 2, 9)
+    screen.print_at(f"Yaw: {drone.pose['yaw']:8.2f}°", 2, 10)
+
+
+def state_printer(drone: Drone):
+    """Draws a box and fills it with information from the drone"""
+    while True:
+        print(drone)
+        sleep(0.02)
+
+
+if __name__ == "__main__":
+    myDrone = Drone(slaveModeEnabled=True)
+    state_printer(myDrone)
